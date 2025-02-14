8f90069ddf0de6cc72a93cfe4295b34f570d016c
ltdat07 <danielle172004@gmail.com>
2025-02-14 23:36:09 +0300

    Add files via upload

---
+++ Mini-Dz01_KPO/Mini-Dz01_KPO/Herbivore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Dz01_KPO
+{
+    /// <summary>
+    /// Абстрактный класс для травоядных животных, добавляет характеристику "добротность".
+    /// </summary>
+    public abstract class Herbivore : Animal
+    {
+        /// <summary>
+        /// Уровень доброты (0-10). Если выше 5 – животное можно использовать в контактном зоопарке.
+        /// </summary>
+        public int Kindness { get; set; }
+
+        protected Herbivore(string name, int food, int number, int kindness)
+            : base(name, food, number)
+        {
+            Kindness = kindness;
+        }
+    }
+}
---
+++ Mini-Dz01_KPO/Mini-Dz01_KPO/Things/Table.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Dz01_KPO
+{
+    /// <summary>
+    /// Класс стол – наследник Thing.
+    /// </summary>
+    public class Table : Thing
+    {
+        public Table(string name, int number) : base(name, number) { }
+    }
+}
